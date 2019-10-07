e87f71fa6d6da6d9ca104e80b6f543a7fcc713f3
rhgkswns21 <rhgkswns@naver.com>
2019-10-07 16:35:14 +0900

    get IMEI & Display List view

--- iosTestApp/ViewController.swift
+++ iosTestApp/ViewController.swift
@@ -8,13 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var NavigationBar: UINavigationItem!
+    @IBOutlet weak var TableView: UITableView!
+    
+    var IDlist = [] as [Any?]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.NavigationBar.title = "Deviece IMEI"
+        self.TableView.dataSource = self
+        imeiGet()
+        print(self.IDlist)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+        print(self.IDlist)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(self.IDlist.count)
+        return self.IDlist.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = (self.IDlist[indexPath.row] as! String)
+        return cell
     }
 
+    func imeiGet() {
+        print("imeiGet")
+        let url = URL(string: "http://111.93.235.82:3105/Identity/entities")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "get"
 
+        let httpession = URLSession.shared
+        let task = httpession.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+            //error 일경우 종료
+            guard error == nil && data != nil else {
+                if let err = error {
+                    print(err.localizedDescription)
+                }
+                return
+            }
+            //data 가져오기
+            if let _data = data {
+                if let strData = NSString(data: _data, encoding: String.Encoding.utf8.rawValue) {
+                    let str = String(strData)
+
+                    let data = str.data(using: .utf8)!
+                    let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [Dictionary<String,Any>]
+                    
+                    for i in (json ?? nil)! {
+                        self.IDlist.append(i["entityId"])
+                    }
+                    DispatchQueue.main.async {
+//                        self.TableView.beginUpdates()
+//                        self.TableView.endUpdates()
+                        self.TableView.reloadData()
+                    }
+                    print(self.IDlist)
+                    print(self.IDlist.count)
+                }
+            }else{
+                print("data nil")
+            }
+        })
+        task.resume()
+    }
 }
 
